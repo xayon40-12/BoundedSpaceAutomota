8c75ef6ad607a57213141272bc8eb673caf2b151
xayon40-12 <touroux.nathan@gmail.com>
2020-10-26 10:29:59 +0100

    add indices

--- src/Rule.hs
+++ src/Rule.hs
@@ -7,3 +7,6 @@ import Store
 
 initialStore :: Vect (Nadd N3 k) Bool -> Store (Vect (Nadd N3 k)) (Fin (Nadd N3 k)) Bool
 initialStore xs = MkStore FO xs
+
+indices :: (Bounded (Fin (Nadd N3 k)),Strengthenable Fin k) => Fin (Nadd N3 k) -> Vect N3 (Fin (Nadd N3 k))
+indices x = down x :> x :> up x :> Nil
