1f4d12d3d476e1a0ac17ab97b9110b903c067e13
xayon40-12 <touroux.nathan@gmail.com>
2020-10-27 16:34:41 +0100

    adding nescessary function for the simulation

--- src/Rule.hs
+++ src/Rule.hs
@@ -4,9 +4,38 @@ import Vect
 import Fin
 import Nat
 import Store
+import Control.Comonad
+import Control.Monad
+import Data.Functor.Rep
 
-initialStore :: Vect (Nadd N3 k) Bool -> Store (Vect (Nadd N3 k)) (Fin (Nadd N3 k)) Bool
+type Na3 k = Nadd N3 k
+type V3 k = Vect (Na3 k)
+type F3 k = Fin (Na3 k)
+type S3 k = Store (V3 k) (F3 k)
+type BSR k = (Rv k,Bf (S k),Sf k)
+
+initialStore :: V3 k Bool -> S3 k Bool
 initialStore xs = MkStore FO xs
 
-indices :: (Bounded (Fin (Nadd N3 k)),Strengthenable Fin k) => Fin (Nadd N3 k) -> Vect N3 (Fin (Nadd N3 k))
+indices :: (Bf (Na3 k),Sf k) => F3 k -> Vect N3 (F3 k)
 indices x = down x :> x :> up x :> Nil
+
+neighbors :: (BSR k) => S3 k Bool -> Vect N3 Bool
+neighbors = experiment indices
+
+isAlive :: (BSR k) => S3 k Bool -> Bool
+isAlive s = case neighbors s of
+    (False :> False :> False :> Nil) -> False
+    (True :> False :> False :> Nil) -> False
+    (True :> True :> True :> Nil) -> False
+    _ -> True
+
+nextGen :: (BSR k) => S3 k Bool -> S3 k Bool
+nextGen = extend isAlive
+
+universe :: (Rv k) => Vect k (Fin k)
+universe = tabulate id
+
+boolToString :: Bool -> String
+boolToString False = "0"
+boolToString True = "1"
