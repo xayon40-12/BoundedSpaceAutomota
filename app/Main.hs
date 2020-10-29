a4ff02f5329b7c8394537fca443ecf19b722deb0
xayon40-12 <touroux.nathan@gmail.com>
2020-10-29 13:42:29 +0100

    working rule 110

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+import Rule
+import Vect
+import Nat
+import Fin
+import Store
+import GHC.Exts (fromList)
+
+type Size = Nadd N10 N4
 
 main :: IO ()
-main = print "hello"
+main = runSimulation init
+    where start :: Vect Size Bool
+          start = fromList $ map (\i -> if i == 0 then False else True) [0,0,0,1,0,0,1,1,0,1,1,1,1,1]
+          init :: Store (Vect Size) (Fin Size) Bool
+          init = initialStore start
